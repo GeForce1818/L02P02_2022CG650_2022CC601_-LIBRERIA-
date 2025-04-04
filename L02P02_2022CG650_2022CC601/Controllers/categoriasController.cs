fb7838d559d67749367e17b4d8790a19b71c56fb
AlexLC96 <soyunfurryowo@gmail.com>
2025-04-03 18:22:24 -0600

    pequeña configuracion a controladores

--- L02P02_2022CG650_2022CC601/Controllers/categoriasController.cs
+++ L02P02_2022CG650_2022CC601/Controllers/categoriasController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using L02P02_2022CG650_2022CC601.Models;
 
 namespace L02P02_2022CG650_2022CC601.Controllers
 {
     public class categoriasController : Controller
     {
+
+        private readonly libreriaDbContext _libreriaDbContext;
+
+        public categoriasController(libreriaDbContext libreriaDbContext)
+        {
+            _libreriaDbContext = libreriaDbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
--- L02P02_2022CG650_2022CC601/Controllers/pedido_encabezadoController.cs
+++ L02P02_2022CG650_2022CC601/Controllers/pedido_encabezadoController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using L02P02_2022CG650_2022CC601.Models;
 
 namespace L02P02_2022CG650_2022CC601.Controllers
 {
     public class pedido_encabezadoController : Controller
     {
+
+        private readonly libreriaDbContext _libreriaDbContext;
+
+        public pedido_encabezadoController(libreriaDbContext libreriaDbContext)
+        {
+            _libreriaDbContext = libreriaDbContext;
+        }
+
         public IActionResult Index()
         {
             return View();

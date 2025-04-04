1ca2f482a979085cf71f8c60fe60139d242dafbc
AlexLC96 <soyunfurryowo@gmail.com>
2025-04-03 18:05:09 -0600

    Controladores

---
+++ L02P02_2022CG650_2022CC601/Controllers/categoriasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2022CG650_2022CC601.Controllers
+{
+    public class categoriasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ L02P02_2022CG650_2022CC601/Controllers/comentarios_librosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2022CG650_2022CC601.Controllers
+{
+    public class comentarios_librosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
